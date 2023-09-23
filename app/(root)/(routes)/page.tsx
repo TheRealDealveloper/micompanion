0a11e2e142fe8757e45f2323d09b88d3cbf0e421
James Gaulhofer <james.gaulhofer@hotmail.com>
2023-09-23 16:11:01 +0200

    auth added

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const RootPage = () => {
   return (
     <div>
-      RootPage (protected)
+      <UserButton afterSignOutUrl='/'></UserButton>
     </div>
   )
 }
